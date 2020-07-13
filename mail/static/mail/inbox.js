54dfdf58e8301296c985a4fc0122ce1aa8c7b989
Patrick Aguiar <patrickaguiar859@gmail.com>
2020-07-13 15:59:18 -0300

    Finish js

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -35,6 +35,25 @@ function get_mail(mailbox){
   .then(response => response.json());
 }
 
+function get_unique_email(email_id){
+  return fetch(`/emails/${email_id}`)
+  .then(response => response.json())
+}
+
+function read_update(email_id, status){
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: status
+    })
+  })
+}
+
+async function archive_update(email_id, status){
+  var init = await fetch(`/emails/${email_id}`, {method: 'PUT', body: JSON.stringify({ archived: status })}); 
+  return init
+}
+
 /* --------------------------------------------------------------------------------- */
 /*CONNECT TO DATABASE*/
 /* --------------------------------------------------------------------------------- */
@@ -46,6 +65,7 @@ function get_mail(mailbox){
 function load_compose_email() {
 
   // Show compose view and hide other views
+  document.querySelector('#email-page-view').style.display = 'none';
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
@@ -105,14 +125,61 @@ function load_mailbox(mailbox) {
         emailRow.append(cols[i])
       }
 
+      emailRow.addEventListener('click', () => load_email_page(email.id)); 
+
       //Append row to mailbox
       document.querySelector('#emails-view').append(emailRow);
     });
   });
   
   // Show the mailbox and hide other views
+  document.querySelector('#email-page-view').style.display = 'none';
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+}
+
+function load_email_page(email_id){
+  get_unique_email(email_id).then(email => {
+    document.querySelector('#email-sender').innerHTML = `Sender: ${email.sender}`;
+    document.querySelector('#email-recipients').innerHTML = `Recipients: ${email.recipients}`;
+    document.querySelector('#email-timestamp').innerHTML = `Timestamp: ${email.timestamp}`;
+    document.querySelector('#email-subject').innerHTML = `Subject: ${email.subject}`;
+    document.querySelector('#email-body').innerHTML = email.body;
+  
+    read_update(email_id, true);
+
+    const archiveButton = document.querySelector('#archive-button');
+    if(email.archived){
+      archiveButton.innerHTML = 'Unarchive';
+      archiveButton.addEventListener('click', () => {
+        archive_update(email_id, false);
+        document.location.reload(true);
+      });
+    }else{
+      archiveButton.innerHTML = 'Archive';
+      archiveButton.addEventListener('click', () => {
+        archive_update(email_id, true);
+        document.location.reload(true);
+      });
+    }
+
+    const replyButton = document.querySelector('#reply-button');
+    replyButton.addEventListener('click', () => reply_email(email_id))
+  })
+
+  // Show the email page and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-page-view').style.display = 'block';
+}
+
+function reply_email(email_id){
+  load_compose_email();
+  get_unique_email(email_id).then(email => {
+    document.querySelector('#compose-recipients').value = email.sender;
+    document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
+    document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote: ${email.body}`;
+  });
 }
 
 function show_message(message, page){
