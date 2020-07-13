8bea105a5e712ca6fe6ab7b1da98f19af5df75e5
Patrick Aguiar <patrickaguiar859@gmail.com>
2020-07-13 11:58:18 -0300

    send_mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -14,7 +14,21 @@ document.addEventListener('DOMContentLoaded', function() {
 /*CONNECT TO DATABASE*/
 /* --------------------------------------------------------------------------------- */
 
+function send_mail(){
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
 
+  return fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+}
 
 /* --------------------------------------------------------------------------------- */
 /*CONNECT TO DATABASE*/
@@ -34,6 +48,18 @@ function load_compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#compose-submit').addEventListener('click', () => {
+    send_mail().then(result => {
+      console.log(result);
+      load_mailbox('inbox');
+      if(result.error){
+        show_message(result.error, document.querySelector('#emails-view'));
+      }else{
+        show_message(result.message, document.querySelector('#emails-view'));
+      }   
+    });
+  });
 }
 
 function load_mailbox(mailbox) {
@@ -44,6 +70,16 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function show_message(message, page){
+  //Create Message Div
+  messageDiv = document.createElement('div');
+  messageDiv.className = 'alert alert-warning';
+  messageDiv.innerHTML = message;
+
+  //Append Message to Page
+  page.append(messageDiv);
 }
 
 /* --------------------------------------------------------------------------------- */
