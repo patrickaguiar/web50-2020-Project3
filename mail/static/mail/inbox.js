bdd5e5c913f3ec209500a2bc806ce4236cd3d182
Patrick Aguiar <patrickaguiar859@gmail.com>
2020-07-13 11:40:24 -0300

    initial project state

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', load_compose_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+/* --------------------------------------------------------------------------------- */
+/*CONNECT TO DATABASE*/
+/* --------------------------------------------------------------------------------- */
+
+
+
+/* --------------------------------------------------------------------------------- */
+/*CONNECT TO DATABASE*/
+/* --------------------------------------------------------------------------------- */
+
+/* --------------------------------------------------------------------------------- */
+/*SHOW RESULTS TO THE USER*/
+/* --------------------------------------------------------------------------------- */
+
+function load_compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+/* --------------------------------------------------------------------------------- */
+/*SHOW RESULTS TO THE USER*/
+/* --------------------------------------------------------------------------------- */
