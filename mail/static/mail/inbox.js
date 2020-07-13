87c1308be8ac3d58014381bc206923cd11ed634f
Patrick Aguiar <patrickaguiar859@gmail.com>
2020-07-13 12:13:24 -0300

    mailbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -30,6 +30,11 @@ function send_mail(){
   .then(response => response.json())
 }
 
+function get_mail(mailbox){
+  return fetch(`/emails/${mailbox}`)
+  .then(response => response.json());
+}
+
 /* --------------------------------------------------------------------------------- */
 /*CONNECT TO DATABASE*/
 /* --------------------------------------------------------------------------------- */
@@ -63,13 +68,51 @@ function load_compose_email() {
 }
 
 function load_mailbox(mailbox) {
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  //Get mails
+  get_mail(mailbox).then(result => {
+    result.forEach(email => {
+      console.log(email);
+
+      //Create Row
+      emailRow = document.createElement('div');
+      if(email.read){
+        emailRow.className = 'row p-3 bg-light mb-1';
+      }else{
+        emailRow.className = 'row p-3 bg-white mb-1';
+      }
+      
+      //Create Cols
+      senderCol = document.createElement('div');
+      subjectCol = document.createElement('div');
+      timestampCol = document.createElement('div');
+
+      //Add classes to cols
+      senderCol.className = 'col-2';
+      subjectCol.className = 'col';
+      timestampCol.className = 'col-2 text-right';
+
+      //Add content to cols
+      senderCol.innerHTML = email.sender;
+      subjectCol.innerHTML = email.subject;
+      timestampCol.innerHTML = email.timestamp;
+
+      //Append Cols to Row
+      cols = [senderCol, subjectCol, timestampCol];
+      for(i=0; i<cols.length; i++){
+        emailRow.append(cols[i])
+      }
+
+      //Append row to mailbox
+      document.querySelector('#emails-view').append(emailRow);
+    });
+  });
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-
-  // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
 
 function show_message(message, page){
